a703b2cef4506ddd89c566e13cd90caa1747d504
Ambulong <zeng.ambulong@gmail.com>
2018-08-15 10:55:56 +0800

    u

--- vsplatego.sh
+++ vsplatego.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service mysqld start || service mysqld start && mysql -u root -ptoor -e 'create database test;';
+service mysqld start || service mysql start && mysql -u root -ptoor -e 'create database test;';
 tar -zxf /var/www/html/hadsky.tar.gz -C /var/www/html;
 rm -rf /var/www/html/hadsky.tar.gz;
 chmod -R 777 /var/www/html;
